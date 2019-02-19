737c20a3f69255c7349cca0731a215ae7f51ea91
yinjihao <yinjihao@yiche.com>
2019-02-19 10:47:28 +0800

    fixxx

--- Test-Three/src/main/java/com/example/mysterious/three/threeActivity.java
+++ Test-Three/src/main/java/com/example/mysterious/three/threeActivity.java
@@ -3,9 +3,6 @@ package com.example.mysterious.three;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.mysterious.grouter_annotation.annotation.annotation.RouterPage;
-
-@RouterPage(path = "com.example.router.three")
 public class threeActivity extends AppCompatActivity {
 
     @Override
